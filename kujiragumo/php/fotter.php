15cee354be2e91cb5e2ce373970adb6cb76a07b4
Aoi1591 <2401906@s.asojuku.ac.jp>
2025-03-23 22:15:42 +0900

    CSS変更

--- kujiragumo/php/fotter.php
+++ kujiragumo/php/fotter.php
@@ -5,8 +5,12 @@
                     住所：〒814-0033  福岡市早良区有田6丁目4-2<br>
                     TEL：092-834-6185<br>FAX：092-834-6186<br>
                     メール：kujiragumoarita@gmail.com
-                    <a href="../docs/program.pdf" target="_blank" class="link">支援プログラムはこちら</a>
                 </div>
+            </div>
+            <div class="fotter-right">
+                    放課後等デイサービスくじらぐも支援プログラムについては
+                    <a href="../docs/program.pdf" target="_blank" class="link">こちら</a>
+                    をご覧ください
             </div>
             <a class="icon" href="https://www.instagram.com/kujiragumo0701/?igsh=dHM2bzA4Z2E0MWxq">
                 <img class="icon-img" src="../img/Instagram_icon.png" alt="Instagram">
